098381ad7437dd2f5f12e0228352af8dde72a834
SoaresLai <161250700+SoaresLai@users.noreply.github.com>
2024-05-15 10:06:30 -0300

    Projeto iniciado

---
+++ Xadrez_Console/Xadrez_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
